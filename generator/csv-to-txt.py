103c908d25607962c757ec0f596e93eab5a802ac
akshitt <akshit.srivastava99@gmail.com>
2019-07-17 21:55:35 +0530

    Last Task

---
+++ generator/csv-to-txt.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import csv
+fields = []
+rows = []
+
+with open('songdata.csv', 'r') as csvfile: 
+    # creating a csv reader object 
+    csvreader = csv.reader(csvfile) 
+      
+    # extracting field names through first row 
+    fields = next(csvreader) 
+  
+    # extracting each data row one by one 
+    for row in csvreader: 
+        rows.append(row) 
+
+text = ''
+j=0
+for i in range(len(rows)):
+	if(rows[i][0] == 'Eminem'):
+		text+=rows[i][3]
+		j=j+1
+
+file = open("lyrics.txt",'w')
+file.write(text)
+file.close()
+
+# print(text,j)
